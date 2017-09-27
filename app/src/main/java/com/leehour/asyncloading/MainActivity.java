21109af332fa45f1ddeac576252deedb34e2d34d
leehour <862215949@qq.com>
2017-09-27 16:01:54 +0800

    异步加载

---
+++ app/src/main/java/com/leehour/asyncloading/MainActivity.java
@@ -0,0 +1,111 @@
+package com.leehour.asyncloading;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ListView mListView;
+    private static String URL = "http://www.imooc.com/api/teacher?type=4&num=30";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUiInvisibility();
+        mListView = (ListView) findViewById(R.id.lv_main);
+        new NewsAsyncTask().execute(URL);
+    }
+
+    class NewsAsyncTask extends AsyncTask<String, Void, List<NewsBean>> {
+
+        @Override
+        protected List<NewsBean> doInBackground(String... params) {
+            return getJsonData(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(List<NewsBean> newsBeen) {
+            super.onPostExecute(newsBeen);
+            NewsAdapter newsAdapter = new NewsAdapter(MainActivity.this, newsBeen,mListView);
+            mListView.setAdapter(newsAdapter);
+        }
+    }
+
+    private List<NewsBean> getJsonData(String url) {
+        List<NewsBean> newsBeanList = new ArrayList<>();
+        try {
+            String jsonString = readStream(new URL(url).openStream());
+            JSONObject jsonObject;
+            NewsBean newsBean;
+            jsonObject = new JSONObject(jsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray("data");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject1 = jsonArray.optJSONObject(i);
+                newsBean = new NewsBean();
+                newsBean.newsIconUrl = jsonObject1.optString("picSmall");
+                newsBean.newsTitle = jsonObject1.optString("name");
+                newsBean.newsContent = jsonObject1.optString("description");
+                newsBeanList.add(newsBean);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return newsBeanList;
+    }
+
+    private String readStream(InputStream is) {
+        InputStreamReader isr;
+        String result = "";
+        try {
+            String line = "";
+            isr = new InputStreamReader(is, "UTF-8");
+            BufferedReader br = new BufferedReader(isr);
+            while ((line = br.readLine()) != null) {
+                result+=line;
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    private void setUiInvisibility() {
+        Window window = getWindow();
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/leehour/asyncloading/NewsBean.java
@@ -0,0 +1,11 @@
+package com.leehour.asyncloading;
+
+/**
+ * Created by leehour on 2017/9/5.
+ */
+
+public class NewsBean {
+    public String newsIconUrl;
+    public String newsTitle;
+    public String newsContent;
+}
